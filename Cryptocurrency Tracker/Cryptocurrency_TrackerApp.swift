bb9746a0453051a6b16fd9a851dab1772f392d1e
Ujjwal <ukarora9@gmail.com>
2024-09-12 16:34:16 +0530

    Initial Commit

---
+++ Cryptocurrency Tracker/Cryptocurrency_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cryptocurrency_TrackerApp.swift
+//  Cryptocurrency Tracker
+//
+//  Created by Ujjwal Arora on 12/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Cryptocurrency_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
